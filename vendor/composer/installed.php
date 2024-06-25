5ef778e23e52efca8f9969297048407afa8f017d
Mark Dock <mark.dock@yahoo.com>
2024-06-25 13:14:25 +0200

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'markdock/imalette',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '41c03770148f572616e13f4800ccec382611e5b1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'markdock/imalette' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '41c03770148f572616e13f4800ccec382611e5b1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
