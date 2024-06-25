0a64e4f11dfa7afd8a53dbbb088ae0469b120eb3
Mark Dock <mark.dock@yahoo.com>
2024-06-25 14:19:56 +0200

    Initial commit

--- src/Imalette.php
+++ src/Imalette.php
@@ -2,10 +2,40 @@
 
 namespace Imalette;
 
+class Palette
+{
+
+}
+
 class Imalette
 {
-    public function GetColor($image)
+    // setup: palette
+    public function getColor($image)
     {
-        return "Hello, World!";
+        $img = imagecreatefrompng($image);
+
+        $width = imagesx($img);
+        $height = imagesy($img);
+        $colors = array();
+
+        for ($y = 0; $y < $height; $y++) {
+            $y_array = array() ;
+
+            for ($x = 0; $x < $width; $x++) {
+                $rgb = imagecolorat($img, $x, $y);
+                $r = ($rgb >> 16) & 0xFF;
+                $g = ($rgb >> 8) & 0xFF;
+                $b = $rgb & 0xFF;
+
+                $x_array = array($r, $g, $b);
+                $y_array[] = $x_array;
+            } 
+            $colors[] = $y_array;
+        }
+        // convert image to RGB
+        // use formula to produce dominant color
+        // find shortest distance between dominant color and palette color
+        // return color in hex
+        return $colors;
     }
 }
